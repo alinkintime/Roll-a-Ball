920cafaecda3721f0797e832026936c35be67a7c
Chad Pearson <G00254194@stu.raritanval.edu>
2018-09-02 18:29:37 -0400

    Roll-a-Ball version 0.3+4
    Tied the camera to the movement of the ball, without spinning it around with the ball.
    
    And also created walls for keep the balls in (forgot to commit the last version)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+
+    }
 }
