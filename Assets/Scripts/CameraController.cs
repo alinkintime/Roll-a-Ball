0641fda82f3b5054003c841f95fdad835b8c5852
Chad Pearson <G00254194@stu.raritanval.edu>
2018-11-02 15:28:04 -0400

    Added more comments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
+    //store a public referance to the Player game object
     public GameObject player;
 
+    //create a vecotr3 offset from the player (a distance ot place the camera from the player)
     private Vector3 offset;
 
 	// Use this for initialization
 	void Start ()
     {
+        //create an offset by subtracting the cameras position form the players position
         offset = transform.position - player.transform.position;
 
 	}
 	
-	// Update is called once per frame
+    // After the 'update()' loop runs. and just before each frame is rendered. 
 	void LateUpdate ()
     {
         transform.position = player.transform.position + offset;
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
-	// Update is called once per frame
+	// Before rendering each frame...
 	void Update ()
     {
+        //Rotate the game object that this script is attached to by 15 on the X axis,
+        //30 on the Y axis, and 45 on the Z axis: multiplied by deltaTime in order to
+        //make it per second
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 	}
 }
